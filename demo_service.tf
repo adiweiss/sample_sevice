7ae6ed1bae217999105eac1d8e74049895acad40
adi.yaskolka <adi.yaskolka@wework.com>
2019-12-11 16:22:04 +0200

    terraform install helm chart

---
+++ demo_service.tf
@@ -0,0 +1,21 @@
+data "helm_repository" "local" {
+  name     = "local"
+  url      = "http://127.0.0.1:8879"
+}
+
+provider kubernetes {
+  config_context_cluster   = "docker-for-desktop-cluster"
+}
+
+provider "helm" {
+  namespace = "default"
+}
+
+resource "helm_release" "demo-service" {
+  name         = "demo-serivce"
+  chart        = "demo-service"
+  version      = "0.1.0"
+  repository   = "local"
+  force_update = true
+}
+
